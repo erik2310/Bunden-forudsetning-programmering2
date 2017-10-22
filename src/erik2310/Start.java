614f8de39528b0581e104ca1c3ee8b3f55924afa
Erik Ruhmanis <erik2310@edu.easj.dk>
2017-10-22 19:33:07 +0200

    Studerende klassen er færdig og testet så den virker.

--- src/erik2310/Start.java
+++ src/erik2310/Start.java
@@ -3,6 +3,8 @@ package erik2310;
 public class Start {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+
+
     }
 }
--- src/erik2310/Studerende.java
+++ src/erik2310/Studerende.java
@@ -1,4 +1,43 @@
 package erik2310;
 
 public class Studerende {
+
+    // Deklarer variabler som skal bruges i klassen, der beskriver de studerende og giver dem default værdier
+    private String navn = "";
+    private int alder = 0;
+    private String by = "";
+    private String skole = "";
+
+    // Default no-arg constructor som bliver kaldt når objektet skal oprettes
+    public Studerende() {
+
+    }
+
+    // Laver en constructor med arguments så man kan lave objekter af studerende og give dem navn, alder, by, skole
+    public Studerende(String navn, int alder, String by, String skole)  {
+        this.navn = navn;
+        this.alder = alder;
+        this.by = by;
+        this.skole = skole;
+    }
+
+    // Laver en metode der returnere navn som String
+    public String getNavn() {
+        return navn;
+    }
+
+    // Laver en metode der returnere alder som int
+    public int getAlder()  {
+        return alder;
+    }
+
+    // Laver en metode der returnere by som String
+    public String getBy()   {
+        return by;
+    }
+
+    // Laver en metode der returnere skole som String
+    public String getSkole() {
+        return skole;
+    }
 }
